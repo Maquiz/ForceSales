0516b0e918b12870bd2a3b05cf90bb9eee809b8c
Thomas A <curioustommy@users.noreply.github.com>
2019-03-02 14:56:24 -0800

    Initial Implementation of Client Components

---
+++ src/data/client/Address.java
@@ -0,0 +1,61 @@
+/**
+ *  @author thomas_a
+ */
+package data.client;
+
+/**
+ * Address is a class that splits the address into multiple components:
+ * 		a. The street address
+ * 		b. The city
+ * 		c. The state
+ * 		d. The zip code
+ * 
+ *  TODO:
+ *  	1. Should I add code to parse the zip code into a ArrayList Integer?
+ */
+
+class Address {
+	private String street_address;
+	private String city;
+	private String state;
+	private String zip_code;
+	
+	
+	public String getStreetAddress() {
+		return street_address;
+	}
+	
+	public void setStreetAddress(String street_address) {
+		this.street_address = street_address;
+	}
+
+	//	Returns the city
+	public String getCity() {
+		return city;
+	}
+
+	//	Assigns the city
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	//	Returns the state
+	public String getState() {
+		return state;
+	}
+
+	//	Assigns the state
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	//	Returns the zip code
+	public String getZipCode() {
+		return zip_code;
+	}
+
+	//	Assigns the zip code
+	public void setZipCode(String zip_code) {
+		this.zip_code = zip_code;
+	}
+}
---
+++ src/data/client/Client.java
@@ -0,0 +1,60 @@
+/**
+ *  @author thomas_a
+ */
+package data.client;
+
+/**
+ * Client is a class that holds information about the current person. It provides the following:
+ * 		a. First name
+ * 		b. Last name
+ * 		c. Email
+ * 		d. Address
+ */
+
+public class Client {
+	private String first_name;
+	private String last_name;
+	private String email;
+	private Address address;
+	
+	// Return the first name
+	public String getFirstName() {
+		return first_name;
+	}
+	
+	// Sets the first_name
+	public void setFirstName(String first_name) {
+		this.first_name = first_name;
+	}
+
+	// Returns the last_name
+	public String getLastName() {
+		return last_name;
+	}
+
+	// Sets the last_name
+	public void setLastName(String last_name) {
+		this.last_name = last_name;
+	}
+
+	// Returns the email
+	public String getEmail() {
+		return email;
+	}
+
+	// Sets the email
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	// Returns the address
+	public Address getAddress() {
+		return address;
+	}
+
+	// Sets the address
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}

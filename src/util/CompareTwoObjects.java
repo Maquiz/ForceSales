ffbfb17d405a4c8e07bbc95072e1e219938d5682
Thomas A. <curioustommy@users.noreply.github.com>
2019-03-08 18:25:12 -0800

    Moved CompareTwoObjects to Separate Folder. Made Interface Public.

--- src/util/CompareTwoObjects.java
+++ src/util/CompareTwoObjects.java
@@ -1,10 +1,10 @@
-package data.client;
+package util;
 
 /*
  * This interface helps abstract the comparisons. It takes the
  * Class A/B and the implemented function would compare between the two
  */
 
-interface CompareTwoObjects<A,B> {
+public interface CompareTwoObjects<A,B> {
 	public boolean compare(A a,B b);
 }

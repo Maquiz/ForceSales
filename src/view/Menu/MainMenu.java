511ea59793c80035da443174920dda10cdf19d88
Thomas A. <curioustommy@users.noreply.github.com>
2019-03-15 00:10:01 -0700

    Implement Main Menu

--- src/view/Menu/MainMenu.java
+++ src/view/Menu/MainMenu.java
@@ -4,17 +4,23 @@ import TasksMenu.Tasks;
 import consoleKit.Menu;
 import consoleKit.MenuStack;
 import data.account.Account;
+import data.account.AccountArray;
+import view.account.AccountMenu;
 import view.client.ClientMenu;
 
 public final class MainMenu extends Menu {
 	private Account account;
+	private AccountArray account_array;
 
 	public MainMenu(String name, MenuStack menus) {
 		super(name, menus);
 		this.account = new Account();
+		this.account_array = new AccountArray();
+		
 		
 		this.add(new ClientMenu("Manage clients", menus, account));
 		this.add(new Tasks(account.getTasks(), this, menus));
+		this.add(new AccountMenu("Manage accounts", account_array, menus));
 	}
 
 }

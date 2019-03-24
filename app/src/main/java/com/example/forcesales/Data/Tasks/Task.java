d53f02c41ed20da35b196e956743d8c52e8ea72f
Thomas A <curioustommy@users.noreply.github.com>
2019-03-23 18:30:55 -0700

    Added Task.java and AbstractTask.java

--- app/src/main/java/com/example/forcesales/Data/Tasks/Task.java
+++ app/src/main/java/com/example/forcesales/Data/Tasks/Task.java
@@ -1,177 +1,43 @@
 package com.example.forcesales.Data.Tasks;
 
-
 import android.os.Parcel;
 import android.os.Parcelable;
 
 import com.example.forcesales.Data.Client.Client;
-import com.example.forcesales.Data.Employee.Employee;
-import com.example.forcesales.Data.Person.Person;
+import com.example.forcesales.Data.Tasks.AbstractTask;
 
-import java.io.Serializable;
 import java.util.Calendar;
-import java.util.Date;
 
-import static java.util.Calendar.getInstance;
+public class Task extends AbstractTask<Client> {
+    public Task() {
+        super();
+    }
 
-/*
- * 
- * 														Task Class
- * This object holds task information such as a task name, is the task complete?, and who is assigned to this task?
- * 
- * 
- * 
- * 
- * Attributes:
- * nameOfTask - String: holds the name of the task.
- * isTaskDone - Boolean: defaults to false, set to true when the task is complete.
- * assigned - Employee: holds employee object of the employee working on the task.
- * assignedDate - Calendar: holds date the task was assigned.
- * dueDate - Calendar: holds due date of task
- * completionDate - Calendar: holds date which the task was completed.
- * 
- * 
- * 
- * 
- * Functions:
- * Constructor - Task(String, Employee, Date) - String input is set to name of task and Employee object is set to assigned Employee. 
- * 							isTaskDone defaults to false. Inputed Date will be set to task's due date.
- * String - getNameOfTask() - Returns name of task.
- * void - setNameOfTask(String) - Changes name of task to input string.
- * boolean - isTaskDone() - Returns boolean. False means the task is not done and True means the task is completed.
- * void - setTaskDone() - Sets isTaskDone attribute to true.
- * Employee - getAssigned() - Returned assigned Employee object.
- * void - changeAssigned(Employee) - Sets assigned Employee to input Employee.
- * Calendar - getCalendarDueDate() - Returns the calendar object for due date, used for back end comparisons.
- * Date - getAssignedDate() - returns date the task was assigned.
- * Date - getDueDate() - returns date the task is due
- * Date - getCompletionDate() - returns date the task was completed.
- * 
- */
+    public Task(String name, String description, Client who, Calendar due) {
+        super(name,description,who,due);
+    }
 
+    @Override
+    public int describeContents(){
+        return 0;
+    }
 
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest,flags);
+    }
 
-public class Task implements Parcelable {
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public Task createFromParcel(Parcel in) {
+            return new Task(in);
+        }
 
-	//Attributes
-	private String nameOfTask;
-	private boolean isTaskDone;
-	private Client assigned;
-	private Calendar assignedDate;
-	private Calendar dueDate;
-	private Calendar completionDate;
-	
-	//Constructor
-	public Task(String name, Client who, Calendar due){
-		this.nameOfTask = name;
-		this.isTaskDone = false;
-		this.assigned = who;
-		this.assignedDate = getInstance();
-		this.dueDate = due;
-		this.completionDate = getInstance();
-		
-	}
+        public Task[] newArray(int size) {
+            return new Task[size];
+        }
+    };
 
-	public String getNameOfTask() {
-		return nameOfTask;
-	}
-
-	public void setNameOfTask(String nameOfTask) {
-		this.nameOfTask = nameOfTask;
-	}
-
-	public boolean isTaskDone() {
-		return isTaskDone;
-	}
-
-	public void setTaskDone() {
-		this.isTaskDone = true;
-		this.completionDate = getInstance();
-	}
-
-	public Client getAssigned() {
-		return assigned;
-	}
-
-	public void changeAssigned(Client assigned) {
-		this.assigned = assigned;
-	}
-	
-	public Calendar getCalendarDueDate() {
-		return this.dueDate;
-	}
-
-	public Date getAssignedDate() {
-		return assignedDate.getTime();
-	}
-
-	public Date getDueDate() {
-		return dueDate.getTime();
-	}
-
-	public Date getCompletionDate() {
-		return completionDate.getTime();
-	}
-
-//	//Attributes
-//	private String nameOfTask;
-//	private boolean isTaskDone;
-//	private Employee assigned;
-//	private Calendar assignedDate;
-//	private Calendar dueDate;
-//	private Calendar completionDate;
-
-	//parcelable methods
-	@Override
-	public int describeContents(){
-		return 0;
-	}
-
-	@Override
-	public void writeToParcel(Parcel dest, int flags){
-		dest.writeString(nameOfTask);
-		dest.writeByte((byte) (isTaskDone ? 1 : 0));
-		dest.writeParcelable(assigned, flags);
-		dest.writeLong(assignedDate.getTimeInMillis());
-		dest.writeLong(dueDate.getTimeInMillis());
-		long temp = 0;
-
-
-		if(completionDate != null) {
-			temp = completionDate.getTimeInMillis();
-		}
-
-
-		dest.writeLong(temp);
-
-	}
-
-	//creator
-	public static final Parcelable.Creator CREATOR = new Parcelable.Creator(){
-		public Task createFromParcel(Parcel in) {
-			return new Task(in);
-		}
-
-		public Task[] newArray(int size) {
-			return new Task[size];
-		}
-	};
-
-	//de-parcel object
-	private Task(Parcel in){
-		nameOfTask = in.readString();
-		isTaskDone = in.readByte() != 0;
-		assigned = in.readParcelable(Client.class.getClassLoader());
-
-		assignedDate = Calendar.getInstance();
-		dueDate = Calendar.getInstance();
-		completionDate = Calendar.getInstance();
-
-		assignedDate.setTimeInMillis(in.readLong());
-		dueDate.setTimeInMillis(in.readLong());
-		completionDate.setTimeInMillis(in.readLong());
-	}
-
-	
-	
+    protected Task(Parcel in) {
+        super(in);
+    }
 }

4ef3440fb3fb8845cde037565eebf64e4f319175
Thomas A <curioustommy@users.noreply.github.com>
2019-03-24 10:22:47 -0700

    Modify TaskList to use AbstractArrayListComparsion

--- app/src/main/java/com/example/forcesales/Data/Tasks/TaskList.java
+++ app/src/main/java/com/example/forcesales/Data/Tasks/TaskList.java
@@ -1,6 +1,8 @@
 package com.example.forcesales.Data.Tasks;
-/*
+/* TODO: Modify Documentation
+ *
  * 												TaskList Class
+ *
  * This class handles storing all the task objects into a list. As well as functions which allow the returning of
  * tasks that are complete, not complete, or assigned to a specific employee.
  * 
@@ -27,96 +29,57 @@ package com.example.forcesales.Data.Tasks;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.example.forcesales.Data.util.AbstractArrayListComparsion;
+import com.example.forcesales.Data.util.CompareTwoObjects;
+
 import java.io.Serializable;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class TaskList implements Parcelable {
-	
-	//Attributes
-	private List<Task> _List;
-	
+public class TaskList extends AbstractArrayListComparsion<Task,TaskList> implements Parcelable {
+
+    // Attributes
+    private CompareTwoObjects<Task,Boolean> compare_tasks = (a, b) -> a.isTaskDone() == b;
+	private CompareTwoObjects<Task,Calendar> compare_date = (a, b) -> {
+	    Calendar a2 = a.getCalendarDueDate();
+        return a2.get(Calendar.DAY_OF_MONTH) == b.get(Calendar.DAY_OF_MONTH)
+                && a2.get(Calendar.MONTH) == b.get(Calendar.MONTH)
+                && a2.get(Calendar.YEAR) == b.get(Calendar.YEAR);
+    };
+
 	//Constructor
 	public TaskList(){
-		_List = new ArrayList<>();
+		super();
+	}
+
+    @Override
+    protected TaskList createEmptyArrayList() {
+        return new TaskList();
+    }
+	
+	public TaskList getCompletedTasks() {
+	    return abstractContains(true, compare_tasks);
 	}
 	
-	public void addTask(Task insert) {
-		_List.add(insert);
+	public TaskList getTasks() {
+	    return abstractContains(false, compare_tasks);
 	}
 	
-	public void getCompletedTasks() {
-		int count = 1;
-		System.out.println("\n");
-		for(int i = 0; i < _List.size(); i++) {
-			if(_List.get(i).isTaskDone() == true) {
-				System.out.println(count + ". " +_List.get(i).getNameOfTask() + " | Assigned to: " + _List.get(i).getAssigned() + " | Completed on: " + _List.get(i).getCompletionDate());
-				count++;
-			}
-		}
-	}
-	
-	public void getTasks() {
-		int count = 1;
-		for(int i = 0; i < _List.size(); i++) {
-			if(_List.get(i).isTaskDone() == false) {
-				System.out.println(count + ". " + _List.get(i).getNameOfTask() + " | Assigned to: " + _List.get(i).getAssigned() + " | Due: " + _List.get(i).getDueDate());
-				count++;
-			}
-		}
-	}
-	
-	public void getAllTasks() {
-		int count = 0;
-		String status;
-		System.out.println("\n");
-		for(int i = 0; i < _List.size(); i++) {
-				if(_List.get(i).isTaskDone() == true) {
-					status = "Yes.";
-				}
-				else {
-					status = "No.";
-				}
-				System.out.println(count + ". " + _List.get(i).getNameOfTask() + " | Assigned to: " + _List.get(i).getAssigned() + " | Assigned on: " + _List.get(i).getAssignedDate() + " | Completion Status: " + status);
-				count++;
-			}
-	}
-	
-	public void getTodaysTasks() {
-		int count = 0;
-		Calendar temp = Calendar.getInstance();
-		System.out.println("\nToday's Tasks:");
-		for(int i = 0; i < _List.size(); i++) {
-			if(_List.get(i).getCalendarDueDate().get(Calendar.DAY_OF_MONTH) == temp.get(Calendar.DAY_OF_MONTH) && _List.get(i).getCalendarDueDate().get(Calendar.MONTH) == temp.get(Calendar.MONTH) && _List.get(i).getCalendarDueDate().get(Calendar.YEAR) == temp.get(Calendar.YEAR) && _List.get(i).isTaskDone() == false) {
-				System.out.println(count + ". " + _List.get(i).getNameOfTask() + " | Assigned to: " + _List.get(i).getAssigned() + " | Assigned on: " + _List.get(i).getAssignedDate());
-				count++;
-			}
-		}
-	}
-	
-	public void removeTask(int index) {
-		_List.remove(index);
-	}
-	
-	public int getSize() {
-		return _List.size();
-	}
-	
-	public Task get(int index) {
-		return _List.get(index);
+	public TaskList getTodaysTasks() {
+        return abstractContains(Calendar.getInstance(),compare_date).getTasks();
 	}
 
 
 	//parcelable methods
 	@Override
-	public int describeContents(){
+	public int describeContents() {
 		return 0;
 	}
 
 	@Override
 	public void writeToParcel(Parcel dest, int flags){
-		dest.writeList(_List);
+		dest.writeList(this);
 	}
 
 	//creator
@@ -132,8 +95,6 @@ public class TaskList implements Parcelable {
 
 	//de-parecel object
 	private TaskList(Parcel in) {
-		_List = new ArrayList<Task>();
-		in.readList(_List, Task.class.getClassLoader());
+		in.readList(this, Task.class.getClassLoader());
 	}
-
 }

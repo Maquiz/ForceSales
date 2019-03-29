46f37fbda23ecda28886c3dabe0ff270a2bcbaca
Thomas A <curioustommy@users.noreply.github.com>
2019-03-29 13:07:42 -0700

    Updated DeveloperMenuActivity

--- app/src/main/java/com/example/forcesales/UI/Developer/DeveloperMenuActivity.java
+++ app/src/main/java/com/example/forcesales/UI/Developer/DeveloperMenuActivity.java
@@ -10,18 +10,24 @@ import android.widget.Toast;
 
 import com.example.forcesales.Data.Client.Client;
 import com.example.forcesales.Data.Developer.Developer;
+import com.example.forcesales.Data.Management.Management;
 import com.example.forcesales.Data.Tasks.Task;
 import com.example.forcesales.R;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 
+/* This is an implementation of the developer menu interface for a developer to interact with.
+ * They will have the ability to create an employee for the SaleForce coorporation and manage
+ * the Issue Tracker.
+ */
+
 public class DeveloperMenuActivity extends AppCompatActivity {
     public static final String TASKLIST = "com.example.forcesales.UI.Developer.DeveloperMenuTest.TASKLIST";
 
     private static final int RETURN_TASK_OBJECT = 1;
 
-    private ArrayList<Developer> developer_list;
+    private Management management;
     private Task test;
 
     @Override
@@ -29,11 +35,14 @@ public class DeveloperMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developer_menu);
 
-        test = new Task("Thomas's Value", "This task has a special meaning!", new Client(), Calendar.getInstance());
+        management = new Management();
+
+//        test = new Task("Thomas's Value", "This task has a special meaning!", new Client(), Calendar.getInstance());
 
         Button sw_ticket = findViewById(R.id.button_dev_sw_ticket);
         sw_ticket.setOnClickListener(v -> {
-            Intent i = null;
+//            Intent i = new Intent(this, );
+//
             Toast t = Toast.makeText(DeveloperMenuActivity.this, "SW TICKET: Need to connect soon", Toast.LENGTH_SHORT);
             t.show();
 
@@ -42,7 +51,7 @@ public class DeveloperMenuActivity extends AppCompatActivity {
 
         Button add_employee = findViewById(R.id.button_dev_add_employee);
         add_employee.setOnClickListener(v -> {
-            Intent i = null;
+//            Intent i = new Intent(this, ShowT);
             Toast t = Toast.makeText(getApplicationContext(), "ADD EMPLOYEE: Need to connect soon", Toast.LENGTH_SHORT);
             t.show();
 
@@ -50,13 +59,13 @@ public class DeveloperMenuActivity extends AppCompatActivity {
 //            startActivityForResult(i,1);
         });
 
-//        Button test_menu = findViewById(R.id.button_dev_test_menu);
-//        test_menu.setOnClickListener(v -> {
+        Button test_menu = findViewById(R.id.button_dev_test_menu);
+        test_menu.setOnClickListener(v -> {
 //            Intent i = new Intent(this, DeveloperMenuTest.class);
 //            i.putExtra(TASKLIST, test);
 //
 //            startActivityForResult(i, RETURN_TASK_OBJECT);
-//        });
+        });
 
 //        test = new Task();
     }
@@ -81,9 +90,6 @@ public class DeveloperMenuActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        Toast t = Toast.makeText(getApplicationContext(), "Returning", Toast.LENGTH_SHORT);
-        t.show();
-
         Intent result = new Intent();
 //        result.putParcelableArrayListExtra()
 
